ae5a81a0cae9e03524d570622581bc75abed5cfc
Joshua Davis <joshua@neeva.co>
2022-07-28 21:22:04 -0700

    Basic design

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,12 +1,28 @@
-import Link from 'next/link'
-
-export default function IndexPage() {
+export default function HomePage() {
   return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
+    <div id="main">
+      <hgroup>
+        <h1>Community Streets</h1>
+        <h2>For Alameda County</h2>
+      </hgroup>
+      <main>
+        <p>
+          Safer streets for people. Vision zero means vision zero. End traffic
+          violence.
+        </p>
+        <p>If you're looking to learn more, please get in touch with us.</p>
+        <form>
+          <label>
+            Your email
+            <input type="email" required placeholder="jane@smith.com" />
+          </label>
+          <label>
+            Your zipcode
+            <input type="zipcode" placeholder="94607" />
+          </label>
+          <input type="submit" value="Connect with us" />
+        </form>
+      </main>
     </div>
-  )
+  );
 }
