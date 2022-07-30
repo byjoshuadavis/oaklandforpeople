a5fad80365bac517aa74c96f9b155b490acadd3d
Joshua Davis <joshua@neeva.co>
2022-07-30 12:36:29 -0700

    Take email & zip

---
+++ site/pages/api/contact.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export default function contact(req) {
+  console.log(req);
+  const { email, zip } = req.query;
+  axios
+    .post(
+      "https://api.airtable.com/v0/appLCQ7R1qR4sb1m4/OaklandForPeople",
+      {
+        records: [
+          {
+            fields: {
+              Email: email,
+              zipcode: zip,
+            },
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.AIR_TABLE_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .catch(console.error);
+}
--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,4 +1,19 @@
+import React from "react";
+import axios from "axios";
 export default function HomePage() {
+  const [email, setEmailState] = React.useState("");
+  const [zip, setZipState] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.get(`/api/contact?zip=${zip}&email=${email}`);
+  }
+  function setEmail(e) {
+    setEmailState(e.currentTarget.value);
+  }
+  function setZip(e) {
+    setZipState(e.currentTarget.value);
+  }
   return (
     <div id="main">
       <hgroup>
@@ -11,14 +26,19 @@ export default function HomePage() {
           violence.
         </p>
         <p>If you're looking to learn more, please get in touch with us.</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>
             Your email
-            <input type="email" required placeholder="jane@smith.com" />
+            <input
+              type="email"
+              required
+              placeholder="jane@smith.com"
+              onChange={setEmail}
+            />
           </label>
           <label>
             Your zipcode
-            <input type="zipcode" placeholder="94607" />
+            <input type="zipcode" placeholder="94607" onChange={setZip} />
           </label>
           <input type="submit" value="Connect with us" />
         </form>
